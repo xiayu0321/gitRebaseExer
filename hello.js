b42d324216ce034ea4696dc605089b97920028fe
zhangqians <2545026281@qq.com>
2016-08-11 10:21:30 +0800

    zhangqian

--- hello.js
+++ hello.js
@@ -13,12 +13,12 @@ function lxy(){
 function liutaojunnan(){
 	console.log('liutaojunann');
 }
-<<<<<<< HEAD
-function zhangqian(){console.log(zhangqian)}
-=======
+
+
+
 
 function zyn(){
     console.log('zyn'); 
 }
->>>>>>> 5b83cac738d44511cb3bb82f4e4b4e25a2532804
+
 function zhangqian(){console.log(zhangqian)}
