21e09e90670719f3bcfa86814cce1386e2d029b8
fanfanqu <18829291202@163.com>
2016-08-11 10:10:52 +0800

    qufan4

--- hello.js
+++ hello.js
@@ -1,12 +1,11 @@
 function (){
 	console.log('hello world');
 }
-<<<<<<< HEAD
+
 function quFan(){
 	console.log("qufan");
 
 }
-=======
 
 function lxy(){
 	//lxy
@@ -14,4 +13,3 @@ function lxy(){
 function liutaojunnan(){
 	console.log('liutaojunann');
 }
->>>>>>> d8bfa470da7d3b4043fd5a47e9a89057f101c27d
